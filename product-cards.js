510ce0ca0adbf976d5c59d4702cb9cc3c7b95b74
Sunjae Kim <sunjaekim@student.unimelb.edu.au>
2025-06-06 16:30:20 +1000

    Added product cards to js file

---
+++ product-cards.js
@@ -0,0 +1,236 @@
+class AppleOatCookies extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Apple Oat Cookies">
+                <h2>Apple Oat Cookies</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('apple-oat-cookies', AppleOatCookies);
+
+
+class AppleCrunch extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Apple Crunch">
+                <h2>Apple Crunch</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('apple-crunch', AppleCrunch);
+
+
+class BdayBone extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="B'Day Bone">
+                <h2>B'Day Bone</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('bday-bone', BdayBone);
+
+
+class BeefBites extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Beef Bites">
+                <h2>Beef Bites</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('beef-bites', BeefBites);
+
+
+class BeefLiver extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Beef Liver">
+                <h2>Beef Liver</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('beef-liver', BeefLiver);
+
+
+class BeefTendons extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Beef Tendons">
+                <h2>Beef Tendons</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('beef-tendons', BeefTendons);
+
+
+class CarrotCrunch extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Carrot Crunch">
+                <h2>Carrot Crunch</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('carrot-crunch', CarrotCrunch);
+
+
+class ChickenFeet extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Chicken Feet">
+                <h2>Chicken Feet</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('chicken-feet', ChickenFeet);
+
+
+class ChickenFeet extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Chicken Feet">
+                <h2>Chicken Feet</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('chicken-feet', ChickenFeet);
+
+
+class LambCrumble extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Lamb Crumble">
+                <h2>Lamb Crumble</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('lamb-crumble', LambCrumble);
+
+
+class Sardines extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Sardines">
+                <h2>Sardines</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('sardines', Sardines);
+
+
+class PeanutButterCookies extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Peanut Butter Cookies">
+                <h2>Peanut Butter Cookies</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('sardines', PeanutButterCookies);
+
+
+class VeggieCrunch extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Veggie Crunch">
+                <h2>Veggie Crunch</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('veggie-crunch', VeggieCrunch);
+
+
+class ChookBugs extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="product-card">
+                <img src="https://sunjaekim4.github.io/INFO20005_A3/images/products-images/apple-oat-cookies.jpg" 
+                    alt="Chook Bugs - Black Soldier Fly Larvae">
+                <h2>Chook Bugs - Black Soldier Fly Larvae</h2>
+                <span class="price">$5.99</span>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        `
+    }
+}
+
+customElements.define('chook-bugs', ChookBugs);
