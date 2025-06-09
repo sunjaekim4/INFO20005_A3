f2ffeb1bbd8d16ce2b14bc2f7664d15ab2522e61
Sunjae Kim <sunjaekim@student.unimelb.edu.au>
2025-06-09 21:34:14 +1000

    Fixed header and footer links

--- header-footer.js
+++ header-footer.js
@@ -1,17 +1,24 @@
+
+
 class Header extends HTMLElement {
   connectedCallback() {
+    const base = window.location.pathname.includes('/shop/') || 
+             window.location.pathname.includes('/about-us/') || 
+             window.location.pathname.includes('/helpful-links/')
+             ? '../' : './';
+
     this.innerHTML = `
         <header>
-                <a href="/index.html" class="header-logo-link">
-                    <img src="/images/farmhouse-full-logo.png" 
+                <a href="${base}index.html" class="header-logo-link">
+                    <img src="${base}images/farmhouse-full-logo.png" 
                         alt="Farmhouse Logo"
                         class="header-logo-img">
                 </a>
                 <nav>
                     <ul class="head-nav-links">
                         <li class="head-nav-link home">
-                            <a href="/index.html">
-                                <img class="paw-icon" src="/images/paw-icon-blue.png">
+                            <a href="${base}index.html">
+                                <img class="paw-icon" src="${base}images/paw-icon-blue.png">
                                 Home
                             </a>
                         </li>
@@ -20,17 +27,17 @@ class Header extends HTMLElement {
                             
                             <input type="checkbox" id="shop-dropdown-active">
                             <label for="shop-dropdown-active" class="head-checkbox shop">
-                                <img class="paw-icon" src="/images/paw-icon-blue.png">
+                                <img class="paw-icon" src="${base}images/paw-icon-blue.png">
                                 <span>Shop</span>
                             </label>
 
 
                             <div class="head-dropdown shop">
-                                <a href="/shop/shop-all.html">Shop All</a>
-                                <a href="/shop/dog-treats.html">Dog Treats</a>
-                                <a href="/shop/cat-treats.html">Cat Treats</a>
-                                <a href="/shop/horses-herbivores.html">Horse & Herbivore Treats</a>
-                                <a href="/shop/poultry-reptiles-natives.html">Poultry, Reptile & Natives Treats</a>
+                                <a href="${base}shop/shop-all.html">Shop All</a>
+                                <a href="${base}shop/dog-treats.html">Dog Treats</a>
+                                <a href="${base}shop/cat-treats.html">Cat Treats</a>
+                                <a href="${base}shop/horses-herbivores.html">Horse & Herbivore Treats</a>
+                                <a href="${base}shop/poultry-reptiles-natives.html">Poultry, Reptile & Natives Treats</a>
                             </div>
                         </li>
 
@@ -38,23 +45,23 @@ class Header extends HTMLElement {
 
                             <input type="checkbox" id="abt-dropdown-active">
                             <label for="abt-dropdown-active" class="head-checkbox about-us">
-                                <img class="paw-icon" src="/images/paw-icon-blue.png">
+                                <img class="paw-icon" src="${base}images/paw-icon-blue.png">
                                 <span>About Us</span>
                             </label>
 
                             <div class="head-dropdown about-us">
-                                <a href="/about-us/our-story.html">Our Story</a>
-                                <a href="/about-us/our-ethics.html">Our Ethics</a>
-                                <a href="/about-us/community.html">Community</a>
-                                <a href="/about-us/where-to-buy.html">Where to Buy</a>
+                                <a href="${base}about-us/our-story.html">Our Story</a>
+                                <a href="${base}about-us/our-ethics.html">Our Ethics</a>
+                                <a href="${base}about-us/community.html">Community</a>
+                                <a href="${base}about-us/where-to-buy.html">Where to Buy</a>
                             </div>
                         </li>
 
                     </ul>
                 </nav>
 
-                <a href="/cart.html" class="cart-icon">
-                    <img src="/images/cart.png" 
+                <a href="${base}cart.html" class="cart-icon">
+                    <img src="${base}images/cart.png" 
                         alt="Cart Icon"
                         class="cart-icon-img">
                 </a>
@@ -76,37 +83,42 @@ function tick(e) {
 
 class Footer extends HTMLElement {
     connectedCallback() {
+        const base = window.location.pathname.includes('/shop/') || 
+             window.location.pathname.includes('/about-us/') || 
+             window.location.pathname.includes('/helpful-links/')
+             ? '../' : './';
+             
         this.innerHTML = `<footer>
         <nav class="footer-navbar">
             <ul class="foot-nav-links">
                 <li>
                     <h2 class="foot-heading">Shop</h2>
                     <ul class="foot-links">
-                        <li><a href="/shop/shop-all.html">Shop All</a></li>
-                        <li><a href="/shop/dog-treats.html">Dog Treats</a></li>
-                        <li><a href="/shop/cat-treats.html">Cat Treats</a></li>
-                        <li><a href="/shop/horses-herbivores.html">Horse & Herbivore Treats</a></li>
-                        <li><a href="/shop/poultry-reptiles-natives.html">Poultry, Reptile & Natives Treats</a></li>
+                        <li><a href="${base}shop/shop-all.html">Shop All</a></li>
+                        <li><a href="${base}shop/dog-treats.html">Dog Treats</a></li>
+                        <li><a href="${base}shop/cat-treats.html">Cat Treats</a></li>
+                        <li><a href="${base}shop/horses-herbivores.html">Horse & Herbivore Treats</a></li>
+                        <li><a href="${base}shop/poultry-reptiles-natives.html">Poultry, Reptile & Natives Treats</a></li>
                     </ul>
                 </li>
 
                 <li>
                     <h2 class="foot-heading">About Us</h2>
                     <ul class="foot-links">
-                        <li><a href="/about-us/our-story.html">Our Story</a></li>
-                        <li><a href="/about-us/our-ethics.html">Our Ethics</a></li>
-                        <li><a href="/about-us/community.html">Community</a></li>
-                        <li><a href="/about-us/where-to-buy.html">Where to Buy</a></li>
+                        <li><a href="${base}about-us/our-story.html">Our Story</a></li>
+                        <li><a href="${base}about-us/our-ethics.html">Our Ethics</a></li>
+                        <li><a href="${base}about-us/community.html">Community</a></li>
+                        <li><a href="${base}about-us/where-to-buy.html">Where to Buy</a></li>
                     </ul>
                 </li>
 
                 <li>
                     <h2 class="foot-heading">Helpful Links</h2>
                     <ul class="foot-links">
-                        <li><a href="/helpful-links/contact-us.html">Contact Us</a></li>
-                        <li><a href="/helpful-links/shipping-returns.html">Shipping & Returns</a></li>
-                        <li><a href="/helpful-links/privacy-policy.html">Privacy Policy</a></li>
-                        <li><a href="/helpful-links/terms-conditions.html">Terms & Conditions</a></li>
+                        <li><a href="${base}helpful-links/contact-us.html">Contact Us</a></li>
+                        <li><a href="${base}helpful-links/shipping-returns.html">Shipping & Returns</a></li>
+                        <li><a href="${base}helpful-links/privacy-policy.html">Privacy Policy</a></li>
+                        <li><a href="${base}helpful-links/terms-conditions.html">Terms & Conditions</a></li>
                     </ul>
                 </li>
 
