0f86abccf4b6a3b94dccd83cec62fa1f5527a286
Sunjae Kim <sunjaekim@student.unimelb.edu.au>
2025-06-10 17:35:10 +1000

    Changed structure and styling of cart page

--- cart.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.product-card .cover-img-link').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-  });
-  link.classList.add('disabled-link');
-});
