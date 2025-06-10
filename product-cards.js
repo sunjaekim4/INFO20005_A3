6ef0989d7f988cc5bf2ee8280433590b6ec41f79
Sunjae Kim <sunjaekim@student.unimelb.edu.au>
2025-06-10 12:25:15 +1000

    Styled layout for individual product pages

--- product-cards.js
+++ product-cards.js
@@ -2,14 +2,16 @@ class AppleOatCookies extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/apple-and-oat-cookies.html">    
+                <a class="cover-img-link" href="../products/apple-and-oat-cookies.html">    
                     <img src="../images/product-images/apple-and-oat-cookies.webp" 
                         alt="Apple Oat Cookies">
                 </a>
 
-                <a href="../products/apple-and-oat-cookies.html"><h2>Apple & Oat Cookies</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/apple-and-oat-cookies.html"><h2>Apple & Oat Cookies</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -22,14 +24,15 @@ class AppleCrunch extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/apple-crunch.html">
+                <a class="cover-img-link" href="../products/apple-crunch.html">
                     <img src="../images/product-images/apple-crunch.webp" 
                         alt="Apple Crunch">
                 </a>
-
-                <a href="../products/apple-crunch.html"><h2>Apple Crunch</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/apple-crunch.html"><h2>Apple Crunch</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -42,14 +45,15 @@ class BdayBone extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/bday-bone.html">
+                <a class="cover-img-link" href="../products/bday-bone.html">
                     <img src="../images/product-images/bday-bone.webp" 
                         alt="B'Day Bone">
                 </a>
-
-                <a href="../products/bday-bone.html"><h2>B'Day Bone</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/bday-bone.html"><h2>B'Day Bone</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -62,14 +66,15 @@ class BeefBites extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/beef-bites.html">
+                <a class="cover-img-link" href="../products/beef-bites.html">
                     <img src="../images/product-images/beef-bites.webp" 
                     alt="Beef Bites">
                 </a>
-
-                <a href="../products/beef-bites.html"><h2>Beef Bites</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/beef-bites.html"><h2>Beef Bites</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -82,14 +87,15 @@ class BeefLiver extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/beef-liver.html">
+                <a class="cover-img-link" href="../products/beef-liver.html">
                     <img src="../images/product-images/beef-liver.webp" 
                         alt="Beef Liver">
                 </a>
-
-                <a href="../products/beef-liver.html"><h2>Beef Liver</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/beef-liver.html"><h2>Beef Liver</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -102,14 +108,15 @@ class BeefTendons extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/beef-tendons.html">
+                <a class="cover-img-link" href="../products/beef-tendons.html">
                     <img src="../images/product-images/beef-tendons.webp" 
                         alt="Beef Tendons">
                 </a>
-
-                <a href="../products/beef-tendons.html"><h2>Beef Tendons</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/beef-tendons.html"><h2>Beef Tendons</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -122,14 +129,15 @@ class CarrotCrunch extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/carrot-crunch.html">
+                <a class="cover-img-link" href="../products/carrot-crunch.html">
                     <img src="../images/product-images/carrot-crunch.webp" 
                         alt="Carrot Crunch">
                 </a>
-
-                <a href="../products/carrot-crunch.html"><h2>Carrot Crunch</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/carrot-crunch.html"><h2>Carrot Crunch</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -142,14 +150,15 @@ class ChickenFeet extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/chicken-feet.html">
+                <a class="cover-img-link" href="../products/chicken-feet.html">
                     <img src="../images/product-images/chicken-feet.webp" 
                         alt="Chicken Feet">
                 </a>
-
-                <a href="../products/chicken-feet.html"><h2>Chicken Feet</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/chicken-feet.html"><h2>Chicken Feet</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -162,14 +171,15 @@ class ChookBugs extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="="../products/chook-bugs.html">
+                <a class="cover-img-link" href="../products/chook-bugs.html">
                     <img src="../images/product-images/chook-bugs.webp" 
                         alt="Chook Bugs - Black Soldier Fly Larvae">
                 </a>
-
-                <a href="../products/chook-bugs.html"><h2>Chook Bugs - Black Soldier Fly Larvae</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/chook-bugs.html"><h2>Chook Bugs - Black Soldier Fly Larvae</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -182,14 +192,15 @@ class LambCrumble extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/lamb-crumble.html">
+                <a class="cover-img-link" href="../products/lamb-crumble.html">
                     <img src="../images/product-images/lamb-crumble.webp" 
                         alt="Lamb Crumble">
                 </a>
-
-                <a href="../products/lamb-crumble.html"><h2>Lamb Crumble</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/lamb-crumble.html"><h2>Lamb Crumble</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -202,14 +213,15 @@ class SardinesFish extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/sardines-fish.html">
+                <a class="cover-img-link" href="../products/sardines-fish.html">
                     <img src="../images/product-images/sardines.webp" 
                         alt="Sardines">
                 </a>
-
-                <a href="../products/sardines-fish.html"><h2>Sardines</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/sardines-fish.html"><h2>Sardines</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -222,14 +234,15 @@ class PeanutButterCookies extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/peanut-butter-cookies.html">
+                <a class="cover-img-link" href="../products/peanut-butter-cookies.html">
                     <img src="../images/product-images/peanut-butter-cookies.webp" 
                         alt="Peanut Butter Cookies">
                 </a>
-
-                <a href="../products/peanut-butter-cookies.html"><h2>Peanut Butter Cookies</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/peanut-butter-cookies.html"><h2>Peanut Butter Cookies</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
@@ -242,14 +255,15 @@ class VeggieCrunch extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="product-card">
-                <a href="../products/veggie-crunch.html">
+                <a class="cover-img-link" href="../products/veggie-crunch.html">
                     <img src="../images/product-images/veggie-crunch.webp" 
                         alt="Veggie Crunch">
                 </a>
-
-                <a href="../products/veggie-crunch.html"><h2>Veggie Crunch</h2></a>
-                <span class="price">$5.99</span>
-                <button class="add-to-cart">Add to Cart</button>
+                <div class="product-labels">
+                    <a class="cover-name-link" href="../products/veggie-crunch.html"><h2>Veggie Crunch</h2></a>
+                    <span class="price">$5.99</span>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
             </div>
         `
     }
